1c7777d07821faa2d9385fabca96f5a28de26304
GongGod <136810939@qq.com>
2023-07-26 22:26:01 +0800

    [Jessie] feat: update basic information

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            1point within 1min ⏱️
           </Link>
         </div>
       </div>
